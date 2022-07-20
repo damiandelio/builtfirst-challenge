e6d65b7ffba78a08eda74584fedb362954d688aa
damiandelio <damiandelio@gmail.com>
2022-07-20 15:14:11 -0300

    adds axios and .env file

---
+++ src/utils/apiCalls.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+/* export const getExample = (): Promise<unknown> => {
+  return axios
+    .get<unknown>("/", {
+      baseURL: API_URL,
+      headers: {},
+      params: {}, // query params
+      data: {}, // body
+    })
+    .then((res) => res.data);
+}; */
